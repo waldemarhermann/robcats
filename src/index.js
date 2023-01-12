5efc973706298ad73d325a4d837c250d28b3c5d6
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-12 09:59:36 +0100

    add import-index.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './container/App';
 import 'tachyons';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
