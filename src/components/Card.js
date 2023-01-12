31a63f23dd4a424381f66b62d65aab60421a4f7d
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-12 10:02:00 +0100

    changed design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 const Card = (props) => {
     return (    
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+        <div className="bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc">
             <img src={`https://robohash.org/test${props.id}?set=set4`} alt="catphoto" />
             <div>
                 <h3>{props.name}</h3>
